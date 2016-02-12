d7cdc96bc0afd4bb504d34eec2cf0411ce4f2bda
Imagine Chiu <imagine10255@gmail.com>
2016-02-12 15:50:19 +0000

    change init

--- Dockerfile
+++ Dockerfile
@@ -4,44 +4,31 @@ MAINTAINER Imagine Chiu<imagine10255@gmail.com>
 
 ENV SSH_ROOT_PASSWORD=P@ssw0rd
 
-# Install base tool
-RUN yum -y install vim tar
-
 
 # Install SSH Service
 RUN yum install -y openssh-server passwd
 RUN sed -ri 's/#UsePAM no/UsePAM no/g' /etc/ssh/sshd_config && \
     echo "${SSH_ROOT_PASSWORD}" | passwd "root" --stdin
 
+
+# Install MariaDB
 RUN echo -e "[mariadb]" >> /etc/yum.repos.d/MariaDB.repo && \
     echo -e "name = MariaDB" >> /etc/yum.repos.d/MariaDB.repo && \
     echo -e "baseurl = http://yum.mariadb.org/10.0/centos6-amd64" >> /etc/yum.repos.d/MariaDB.repo && \
     echo -e "gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB" >> /etc/yum.repos.d/MariaDB.repo && \
-    echo -e "gpgcheck=1" >> /etc/yum.repos.d/MariaDB.repo
-
-RUN yum -y install MariaDB-Galera-server MariaDB-client galera
+    echo -e "gpgcheck=1" >> /etc/yum.repos.d/MariaDB.repo && \
+    yum -y install MariaDB-Galera-server MariaDB-client galera
 
 # Copy files for setting
 ADD ./docker /opt/
 
 
-RUN $(which cp) /usr/share/mysql/my-medium.cnf /etc/my.cnf.bak && \
-    $(which cp) /usr/share/mysql/wsrep.cnf /etc/my.cnf.d/ && \
-    $(which cp) -frp /opt/config/mysql/my.cnf /etc/my.cnf && \
-    $(which cp) -frp /var/lib/mysql /home/mysql
-
-
 # Create Base Enter Cont Command
-RUN chmod 755 /opt/bash/init-bashrc.sh && echo "/opt/bash/init-bashrc.sh" >> /root/.bashrc && \
-    echo 'export PATH="/root/.composer/vendor/bin:$PATH"' >> /root/.bashrc
+RUN chmod 755 /opt/bash/init-bashrc.sh && echo "/opt/bash/init-bashrc.sh" >> /root/.bashrc
 
 
 # Setting DateTime Zone
 RUN cp -p /usr/share/zoneinfo/Asia/Taipei /etc/localtime
-
-
-# Setup default path
-WORKDIR /home
 
 
 # Private expose
@@ -54,4 +41,3 @@ VOLUME ["/home/mysql","/home/tmp"]
 
 # Start run shell
 CMD ["bash"]
-
--- docker/bash/init-bashrc.sh
+++ docker/bash/init-bashrc.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-MYSQL_DATA_PATH="/home/mysql"
+MYSQL_DATA_PATH=/home/mysql
 MYSQL_ROOT_PASSWORD=P@ssw0rd
-
-MKDIR="$(which mkdir)"
-RM="$(which rm)"
-MV="$(which mv)"
-SH="$(which sh)"
-CP="$(which cp)"
+MYSQL_PASSWORD_CHECK=0
 
 # Check Service to start
 function service_start()
@@ -23,12 +18,34 @@ function service_start()
 
 
 # Check & Copy DB Files
-if [ "`ls -A $MYSQL_DATA_PATH`" = "" ]; then
-  echo "$DIRECTORY not database files, run copy default database files to here"
-  $CP -rp /var/lib/mysql/* $MYSQL_DATA_PATH/
-  $(which service) mysql start
-  $(which mysqladmin) -uroot password ${MYSQL_ROOT_PASSWORD}
-  $(which mysql) -uroot -p${MYSQL_ROOT_PASSWORD} -e"GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION;flush privileges;"
+if [ -f "/usr/share/mysql/my-medium.cnf" ]; then
+    echo "move my-medium.cnf /etc/my.cnf.bak"
+    mv -f /usr/share/mysql/my-medium.cnf /etc/my.cnf.bak
 fi
 
-service_start
+
+if [ -f "/usr/share/mysql/wsrep.cnf" ]; then
+    echo "move wsrep.cnf /etc/my.cnf.d/wsrep.cnf"
+    mv -f /usr/share/mysql/wsrep.cnf /etc/my.cnf.d/wsrep.cnf
+fi
+
+
+if [ -f "/opt/config/mysql/my.cnf" ]; then
+    echo "move default custom config /etc/my.cnf"
+    mv -f /opt/config/mysql/my.cnf /etc/my.cnf
+
+    chown -R mysql.mysql /var/lib/mysql
+    chown -R mysql.mysql ${MYSQL_DATA_PATH}
+fi
+
+
+if [ "`ls -A $MYSQL_DATA_PATH`" = "" ]; then
+    echo "${MYSQL_DATA_PATH} not database files, run copy default database files to here"
+    cp -frp /var/lib/mysql/* $MYSQL_DATA_PATH/
+
+    service_start
+    mysqladmin -uroot password ${MYSQL_ROOT_PASSWORD}
+    mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e"GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION;flush privileges;"
+else
+    service_start
+fi
