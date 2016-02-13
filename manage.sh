0e8af31b7bc6deba71e90e30375220b4ce91d853
Imagine Chiu <imagine10255@gmail.com>
2016-02-13 02:55:39 +0000

    modify Dockerfile tools manage.sh

--- manage.sh
+++ manage.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Program:
 
-CONTAINERID="${1}"
 IMAGES_NAME=imagine10255/centos6-mariadb:latest
 clear
 
@@ -9,7 +8,8 @@ until [ "$NUM" == "q" ] #輸入文字q 則離開
 do
     # View
     echo -e "
-Dockerfile Tools\n
+Dockerfile Tools
+
 [container manage]   
 cc) create-container
 ec) enter-container
@@ -20,36 +20,45 @@ bi) build-images
 pi) push-images
 di) delete-images
 "
-    read NUM
+    read -p "Enter your choice: " NUM
     case $NUM in
     cc)
         docker run -it ${IMAGES_NAME} bash
         exit;
     ;;
     ec)
-        if [ "$CONTAINERID" == "" ]; then
-           echo ">> please keyin {Container-id or Name}";
-           exit;
-        fi
-        docker exec -it ${CONTAINERID} bash
+        docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Image}}\t{{.Ports}}"
+        echo ""
+        read -p "Enter Container ID or Name: " CONTAINERID
+        case $CONTAINERID in
+        *)
+            docker exec -it ${CONTAINERID} bash
+        ;;esac
     ;;
     ic)
-        if [ "$CONTAINER" == "" ]; then
-           echo ">> please enter container new-name";
-           exit;
-        fi
+        docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Image}}\t{{.Ports}}"
+        echo ""
+        read -p "Rename Container: " NEW_CONTAINER_NAME
+        case $NEW_CONTAINER_NAME in
+        *)
+            if [ "$NEW_CONTAINER_NAME" == "" ]; then
+               echo ">> please enter container new-name";
+               exit;
+            fi
+            
+             # create docker-container
+             docker run -idt \
+             --name "$NEW_CONTAINER_NAME" \
+             -p 3306:3306 \
+             -p 22:3322 \
+             -v /home/tmp:/home/tmp \
+             -v /home/mysql:/home/mysql \
+             ${IMAGES_NAME}
 
-        # create docker-container
-        docker run -idt \
-        --name "$CONTAINER" \
-        -p 3306:3306 \
-        -p 22:3322 \
-        -v /home/tmp:/home/tmp \
-        -v /home/mariadb:/home/mariadb \
-        ${IMAGES_NAME}
-
-        # enter docker-container
-        docker exec -it ${CONTAINERID} bash
+             # enter docker-container
+             docker exec -it "$NEW_CONTAINER_NAME" bash
+ 
+        ;;esac
     ;;
     bi)
         docker build -t ${IMAGES_NAME} .
@@ -68,4 +77,3 @@ di) delete-images
     *) 
         echo ">> Please keyin true option" && exit ;;esac	
 done
-
